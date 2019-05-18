2b69a4745d71bcfc92682d12d772f90b4392c438
yuusaku-yoshida <50648319+yuusaku-yoshida@users.noreply.github.com>
2019-05-18 16:10:29 +0900

    [WIP]
    コメント機能無しpictweet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   root  'tweets#index'
-  get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
-  get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
-  post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
